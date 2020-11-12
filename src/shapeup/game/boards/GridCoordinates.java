dc566b20c0d55fa8d0de74b73d8968c23120c14d
Maxime Huyghe <maxime.huyghe@etu.u-pec.fr>
2020-11-12 19:54:51 +0100

    GridCoordinates fixes

--- src/shapeup/game/boards/GridCoordinates.java
+++ src/shapeup/game/boards/GridCoordinates.java
@@ -13,18 +13,34 @@ public class GridCoordinates {
   }
 
   public boolean isAdjacentTo(GridCoordinates other) {
-    boolean adj = false;
-    if (Math.abs(other.x - this.x) == 1 || Math.abs(other.y - this.y) == 1) {
-      adj = true;
-    }
-    return adj;
+    int xDiff = Math.abs(other.x - this.x);
+    int yDiff = Math.abs(other.y - this.y);
+    return xDiff <= 1 && yDiff <= 1 && (xDiff == 1 ^ yDiff == 1);
   }
 
-  public boolean equals(GridCoordinates other) {
-    boolean equ = false;
-    if (other.x == this.x && other.y == this.y) {
-      equ = true;
-    }
-    return equ;
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+
+    GridCoordinates that = (GridCoordinates) o;
+
+    if (x != that.x) return false;
+    return y == that.y;
+  }
+
+  @Override
+  public int hashCode() {
+    int result = x;
+    result = 31 * result + y;
+    return result;
+  }
+
+  @Override
+  public String toString() {
+    return "GridCoordinates{" +
+            "x=" + x +
+            ", y=" + y +
+            '}';
   }
 }
