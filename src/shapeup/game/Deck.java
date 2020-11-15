ce57e430eb5e3b8ae29a5569944ac03ae0430d34
Axel Calixte <axelcalixte@disroot.org>
2020-11-15 15:39:00 +0100

    implement Deck class

--- src/shapeup/game/Deck.java
+++ src/shapeup/game/Deck.java
@@ -1,20 +1,54 @@
 package shapeup.game;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 public final class Deck {
-  private final ArrayList<Card> cards;
+    private final ArrayList<Card> cards = new ArrayList<>();
 
-  public Deck() {
-    // Cards will be generated here
-    this.cards = new ArrayList<>();
-  }
+    public Deck() {
+        this.generateDeck();
+    }
 
-  public Card drawCard() {
-    return null;
-  }
+    public void generateDeck() {
+        for (Shape s : Shape.values()
+        ) {
+            for (Color c : Color.values()
+            ) {
+                for (Filledness f : Filledness.values()
+                ) {
+                    this.cards.add(new Card(c, s, f));
+                }
+            }
+        }
+        Collections.shuffle(this.cards);
+    }
 
-  public int cardsLeft() {
-    return 0;
-  }
+    public Card drawCard() {
+        if (this.cards.size() == 0) {
+            return null;
+        }
+        return this.cards.remove(this.cards.size() - 1);
+    }
+
+    public int cardsLeft() {
+        return this.cards.size();
+    }
+
+    @Override
+    public String toString() {
+        return "Deck{" +
+                 cards +
+                '}';
+    }
+
+    public static void main(String[] args) {
+        Deck deck = new Deck();
+        Card drawn = deck.drawCard();
+        Card drawn2 = deck.drawCard();
+        System.out.println(drawn);
+        System.out.println(drawn2);
+        System.out.println(deck.cardsLeft());
+        System.out.println(deck);
+    }
 }
