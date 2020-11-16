c8a0599b69bce602d4091779deecec28fc09aa88
Maxime Huyghe <maxime.huyghe@etu.u-pec.fr>
2020-11-16 22:10:37 +0100

    Small Deck fixes

--- src/shapeup/game/Deck.java
+++ src/shapeup/game/Deck.java
@@ -5,39 +5,35 @@ import java.util.Collections;
 import java.util.Optional;
 
 public final class Deck {
-    private final ArrayList<Card> cards = new ArrayList<>();
+  private final ArrayList<Card> cards = new ArrayList<>(Shape.values().length * Color.values().length * Filledness.values().length);
 
-    public Deck() {
-        this.generateDeck();
-    }
-
-    public void generateDeck() {
-        for (Shape s : Shape.values()) {
-            for (Color c : Color.values()) {
-                for (Filledness f : Filledness.values()) {
-                    this.cards.add(new Card(c, s, f));
-                }
-            }
+  public Deck() {
+    for (Shape s : Shape.values()) {
+      for (Color c : Color.values()) {
+        for (Filledness f : Filledness.values()) {
+          this.cards.add(new Card(c, s, f));
         }
-        Collections.shuffle(this.cards);
+      }
     }
+    Collections.shuffle(this.cards);
+  }
 
-    public Optional<Card> drawCard() {
-        if (this.cards.size() == 0) {
-            return Optional.empty();
-        }
-        Card drawnCard = this.cards.remove(this.cards.size() - 1);
-        return Optional.of(drawnCard);
+  public Optional<Card> drawCard() {
+    if (this.cards.size() == 0) {
+      return Optional.empty();
     }
+    Card drawnCard = this.cards.remove(this.cards.size() - 1);
+    return Optional.of(drawnCard);
+  }
 
-    public int cardsLeft() {
-        return this.cards.size();
-    }
+  public int cardsLeft() {
+    return this.cards.size();
+  }
 
-    @Override
-    public String toString() {
-        return "Deck{" +
-                cards +
-                '}';
-    }
+  @Override
+  public String toString() {
+    return "Deck{" +
+            cards +
+            '}';
+  }
 }
