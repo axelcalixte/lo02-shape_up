1024864dce6106ee86099e5680df48b95e75d272
Axel Calixte <axelcalixte@disroot.org>
2021-01-07 17:13:14 +0100

    documentation for Coordinates

--- src/shapeup/game/boards/Coordinates.java
+++ src/shapeup/game/boards/Coordinates.java
@@ -1,22 +1,47 @@
 package shapeup.game.boards;
 
+/**
+ * A class representing a pair of cartesian coordinates for the boards of the Shape Up! game.
+ */
 public class Coordinates {
   protected final int x;
   protected final int y;
 
+  /**
+   * Constructs a pair of cartesian Coordinates.
+   *
+   * @param x - an int for the horizontal value of the coordinates.
+   * @param y - an int for the vertical value of the coordinates.
+   */
   public Coordinates(int x, int y) {
     this.x = x;
     this.y = y;
   }
 
+  /**
+   * Returns the x value of the coordinates.
+   *
+   * @return the x value of the coordinates.
+   */
   public int getX() {
     return x;
   }
 
+  /**
+   * Returns the y value of the coordinates.
+   *
+   * @return the y value of the coordinates.
+   */
   public int getY() {
     return y;
   }
 
+  /**
+   * Indicates whether some other object is "equal to" a coordinate.
+   *
+   * @param o - the object to compare the coordinates to.
+   * @return true if this coordinate is the same as the obj argument; false otherwise.
+   */
   @Override
   public boolean equals(Object o) {
     if (this == o) return true;
@@ -28,6 +53,11 @@ public class Coordinates {
     return y == that.y;
   }
 
+  /**
+   * Returns the hash of the coordinates.
+   *
+   * @return an int representing the hash.
+   */
   @Override
   public int hashCode() {
     int result = x;
@@ -35,6 +65,11 @@ public class Coordinates {
     return result;
   }
 
+  /**
+   * Returns a string representation of the coordinates.
+   *
+   * @return a string representation of the coordinate.
+   */
   @Override
   public String toString() {
     return "GridCoordinates{" +
