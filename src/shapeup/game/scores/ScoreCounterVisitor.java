049edc29e59dd2cd69517b82ce7176a74eab4cfc
Maxime Huyghe <maxime.huyghe@etu.u-pec.fr>
2021-01-09 18:50:50 +0100

    documentation for the scores package

--- src/shapeup/game/scores/ScoreCounterVisitor.java
+++ src/shapeup/game/scores/ScoreCounterVisitor.java
@@ -4,8 +4,23 @@ import shapeup.game.Card;
 import shapeup.game.boards.CircleBoard;
 import shapeup.game.boards.GridBoard;
 
+/**
+ * Counts the scores for a game of Shape Up!.
+ */
 public interface ScoreCounterVisitor {
+  /**
+   * Count the given victory card's score on a Gridboard.
+   * @param board the board
+   * @param victoryCard the victory card
+   * @return the score
+   */
   int countGridBoard(GridBoard board, Card victoryCard);
 
+  /**
+   * Count the given victory card's score on a CircleBoard.
+   * @param board the board
+   * @param victoryCard the victory card
+   * @return the score
+   */
   int countCircleBoard(CircleBoard board, Card victoryCard);
 }
